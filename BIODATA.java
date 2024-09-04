9a123a6cb57746d17a64d474f6e1d7e3888a4dc2
achmadanval <anvalalan@gmail.com>
2024-09-04 11:05:17 +0700

    biodata kelompok ceria

---
+++ BIODATA.java
@@ -0,0 +1,18 @@
+public class BIODATA {
+
+    public static void main(String[] args) {
+        System.out.println("---------------------------------------------");
+        System.out.println("Data Mahasiswa Teknologi Informasi POLINEMA ");
+        System.out.println("----------------------------------------------");
+        System.out.println("Nama       : Achmad Anval Adhiem Allain");
+        System.out.println("NIM        : 244107020039");
+        System.out.println("kelas      : 1B");
+        System.out.println("NO.ABSEN   : 02");
+        System.out.println("PRODI      : D4 Teknik Informatika");
+        System.out.println("ALAMAT     : Pecalukan,Prigen<pasuruan");
+        System.out.println("NO.HP      : 082111409962");
+        System.out.println("HOBI       : Olahraga");
+    }
+
+    
+}
